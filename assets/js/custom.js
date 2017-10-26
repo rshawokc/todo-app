908146f104284aef709ebdf52a59f518dd30be23
Rick Shaw <rick@ricksconcepts.com>
2017-10-26 15:38:32 -0500

    Updated js to toggle the plus icon to hide the input

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,14 +11,15 @@ $('ul').on('click', 'span', function(event) {
   event.stopPropagation();
 });
 
+//Grabs new to-do input, Clears input, Adds new li and adds to ul
 $("input[type='text']").keypress(function(event) {
   if (event.which === 13) {
-   //Grabs new to-do input
    var todoText = $(this).val();
-   //Clears input
    $(this).val('');
-   //Adds new li and adds to ul
    $('ul').append('<li><span><i class="fa fa-trash" aria-hidden="true"></i></span> ' + todoText + '</li>');
   }
 });
 
+$('.fa-plus').click(function() {
+  $("input[type='text']").fadeToggle();
+});
